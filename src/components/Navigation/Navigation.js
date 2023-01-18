de4ca5b9ea6f326eb07318d0e63813e3ae5119dc
YuqingWangCG <yuqing_wang@protonmail.com>
2023-01-18 12:32:29 +0100

    nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,18 +9,18 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
       		className='f3 link dim black underline pa3 pointer'>Sign out</p>
        	</nav>
        );
-	} else {
-		return(
-			<nav style={{display:'flex', justifyContent: 'flex-end'}}>
-      			<p 
-      			onClick={()=> onRouteChange('signin')}
-      		 	className='f3 link dim black underline pa3 pointer'>Sign in</p>
-      		 	{/*<p 
-      			onClick={()=> onRouteChange('register')}
-      		 	className='f3 link dim black underline pa3 pointer'>Register</p>*/}
-      		</nav>
-		);
-	}
+	// } else {
+	// 	return(
+	// 		<nav style={{display:'flex', justifyContent: 'flex-end'}}>
+     //  			<p 
+     //  			onClick={()=> onRouteChange('signin')}
+     //  		 	className='f3 link dim black underline pa3 pointer'>Sign in</p>
+     //  		 	<p 
+     //  			onClick={()=> onRouteChange('register')}
+     //  		 	className='f3 link dim black underline pa3 pointer'>Register</p>
+     //  		</nav>
+	// 	);
+	// }
 }
 
 
