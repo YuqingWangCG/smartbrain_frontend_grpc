9a5c49f68de5880f8cd0cb688a736ffaf82b60a3
YuqingWangCG <yuqing_wang@protonmail.com>
2023-01-18 16:07:57 +0100

    clear

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,12 +8,17 @@ const ImageLinkForm = ({inputURL, onInputChange, onPictureSubmit, onClearSubmit}
 			  {`This Smart Brain will detect face in your pictures. Let's give it a try!`}
 			</p>
             <div className='backgroundpattern pa4 br3 shadow-5 center' style={{width:'900px'}}>
-               <input className='f4 pa2 w-60' type='text' placeholder='Please paste a jpg link of your picture:' value={inputURL} onChange={onInputChange}/>
+               <input 
+               	className='f4 pa2 w-60' 
+               	type='text' 
+               	placeholder='Please paste a jpg link of your picture:' 
+               	value={inputURL} 
+               	onChange={onInputChange}/>
                <button 
                	className='w-20 grow f4 ph3 pv2 dib white bg-light-purple pointer'
                	onClick={onPictureSubmit}>Detect</button>
                <button 
-               	className='w-20 grow f4 ph3 pv2 dib white bg-light-purple pointer'
+               	className='w-20 grow f4 ph3 pv2 dib white bg-light-blue pointer'
                	onClick={onClearSubmit}>Clear</button>
             </div>
 		</div>
